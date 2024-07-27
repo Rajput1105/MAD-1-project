235d429e8d3013defab5eff7f32264f5dc4f363b
Rajput1105 <rahulrajput110503@gmail.com>
2024-07-27 13:02:09 +0530

    flag
    flag

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ logging.basicConfig(level=logging.DEBUG)
 def get_db_connection():
     conn = sqlite3.connect('mydatabase.db')
     conn.row_factory = sqlite3.Row
-    # conn.execute("PRAGMA foreign_keys = ON;")
+    conn.execute("PRAGMA foreign_keys = ON;")
 
     return conn
 
@@ -91,7 +91,7 @@ def add_sponsor():
             conn = get_db_connection()
             cur = conn.cursor()
             #Now insert Data
-            cur.execute("INSERT INTO sponsor (company_name, budget, industry, username, password) VALUES (?, ?, ?, ?, ?)", (company_name, budget, industry, username, password))
+            cur.execute("INSERT INTO sponsor (company_name, budget, industry, username, password) VALUES (?, ?, ?, ?, ?,?)", (company_name, budget, industry, username, password,0))
             conn.commit()
             msg = 'Sponsor added successfully!'
     except Exception as e:
@@ -117,7 +117,7 @@ def add_influencer():
             conn = get_db_connection()
             cur = conn.cursor()
             #Now inserting data
-            cur.execute("INSERT INTO influencer (influencer_name, target_audience, youtube, instagram, twitter, username, password) VALUES (?, ?, ?, ?, ?, ?, ?)", (influencer_name, target_audience, youtube, instagram, twitter, username, password))
+            cur.execute("INSERT INTO influencer (influencer_name, target_audience, youtube, instagram, twitter, username, password,Earning) VALUES (?, ?, ?, ?, ?, ?, ?,?)", (influencer_name, target_audience, youtube, instagram, twitter, username, password,0))
             conn.commit()
             msg = 'Influencer added ! Thank You !'
     except Exception as e:
@@ -156,62 +156,143 @@ def validate_influencer():
     cur = conn.cursor()
     cur.execute("SELECT * FROM influencer WHERE username = ?", (username,))
     influencer = cur.fetchone()
-    cur.execute("SELECT target_audience FROM influencer WHERE username = ?", (username,))
-    T_a = cur.fetchone()
+    cur.execute("SELECT * FROM campaigns ")
+    campaigns = cur.fetchall()
     conn.close()
     if influencer and influencer['password']==Inf_password:
-        return render_template('influ_dashboard.html' ,influencer=influencer , T_a=T_a) 
+        return render_template('influ_dashboard.html' ,influencer=influencer ,campaigns=campaigns ) 
     else:
         return "login failed!"
 
 
 
-@app.route('/create_campaigns/', methods=['GET', 'POST'])
+@app.route('/accept/<int:campaign_id>/<int:influencer_id>/', methods=['POST'])
+def accept(campaign_id,influencer_id):
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        cur.execute('UPDATE campaigns SET influencer_decision = ? WHERE id = ?', ('accept' ,campaign_id,))
+        conn.commit()
+        conn.close
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM campaigns')
+        campaigns = cur.fetchall()
+        conn.close()
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM influencer WHERE id = ?", (influencer_id,))
+        influencer = cur.fetchone()
+        conn.close()
+        return render_template('influ_dashboard.html' ,influencer=influencer ,campaigns=campaigns ) 
+
+
+
+@app.route('/reject/<int:campaign_id>/<int:influencer_id>/', methods=['POST'])
+def reject(campaign_id,influencer_id):
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('UPDATE campaigns SET influencer_decision = ? WHERE id = ?', ('reject',campaign_id,))
+        conn.commit()
+        conn.close
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM campaigns')
+        campaigns = cur.fetchall()
+        conn.close()
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM influencer WHERE id = ?", (influencer_id,))
+        influencer = cur.fetchone()
+        conn.close()
+        return render_template('influ_dashboard.html' ,influencer=influencer ,campaigns=campaigns ) 
+
+
+
+
+
+
+
+
+@app.route('/create_campaigns/', methods=['GET'])
 def create_campaigns():
-        return render_template("create_campaigns.html")
+        
 
-# @app.route('/ADD/',methods=['POST'])
-# def ADD():
-#      if request.method=='POST':
-#         try:
+        return render_template('create_campaigns.html')
+
+
+@app.route('/ADD/',methods=['POST'])
+def ADD():
+     if request.method=='POST':
+        
                
-#             sponsor_id = request.form['id']
-#             name = request.form['name']
-#             description = request.form['description']
-#             budget = request.form['budget']
-#             start_date = request.form['start-date']
-#             end_date = request.form['end-date']
+            sponsor_id = request.form['sponsor_id']
+            name = request.form['campaign_name']
+            description = request.form['description']
+            budget = request.form['budget']
+            start_date = request.form['start-date']
+            end_date = request.form['end-date']
+            visibility=request.form['visibility']
+            conn = get_db_connection()
+            cur = conn.cursor()
+            cur.execute('INSERT INTO campaigns (name, description, budget ,start_date,end_date,visibility,sponsor_id,image,status,flagged,influencer_decision) VALUES (?, ?, ?, ?, ?,?,?,?,?,?)',
+                            (name, description, budget,start_date,end_date, visibility,sponsor_id,'null','Active',0,'pending'))
+            conn.commit()
+            conn = get_db_connection()
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM campaigns')
+            campaigns = cur.fetchall()
+            conn.close()
+            return render_template('sponsor_das.html',campaigns=campaigns)
+
+@app.route('/viewads/<int:campaign_id>', methods=['POST'])
+def viewads(campaign_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM ads WHERE campaign_id = ?", (campaign_id,))
+    ads = cur.fetchall()
+    cur.close()
 
 
-@app.route('/ADD', methods=['POST'])
-def add_campaign():
+
+    return render_template('campaign_ad.html',ads=ads,campaign_id=campaign_id)
+
+
+
+@app.route('/ads/<int:campaign_id>', methods=['POST'])
+def ads(campaign_id):
     try :
-        name = request.form['campaign_name']
-        sponsor_id = request.form['sponsor_id']
-        start_date = request.form['start-date']
-        end_date = request.form['end-date']
+        ad_name = request.form['ad_name']
+        
+        terms = request.form['terms']
+        
         budget = request.form['budget']
         # image = request.files['image'] 
         description = request.form['description']
-
+        influencer_name=request.form['influencer_name']
         conn = sqlite3.connect('mydatabase.db')
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
        
         
         cur = conn.cursor()
-        cur.execute('INSERT INTO campaigns (sponsor_id, name, description, budget, start_date, end_date, status,image) VALUES (?, ?, ?, ?, ?, ?, ?,?)',
-                            (sponsor_id, name, description, budget, start_date, end_date, 'Active','null'))
+        cur.execute('INSERT INTO ads (ad_name, description, budget , terms,campaign_id,influencer_name) VALUES (?, ?, ?, ?, ?,?)',
+                            (ad_name, description, budget, terms,campaign_id,influencer_name))
         conn.commit()
         conn.close()
-        logging.info("Campaign added successfully")
+        logging.info("ad added successfully")
         conn = get_db_connection()
         cur = conn.cursor()
+        cur.execute('SELECT * FROM ads')
+        ads = cur.fetchall()
         cur.execute('SELECT * FROM campaigns')
-        campaigns = cur.fetchall()
+        campaigns=cur.fetchall()
+
         conn.close()
         
-        return render_template('create_campaigns.html',campaigns=campaigns)
+        return render_template('create_campaigns.html',campaigns=campaigns,ads=ads)
     except Exception as e:
         logging.error(f"Error: {e}")
         return render_template('sponsor_das.html')
@@ -228,19 +309,186 @@ def info():
 
 
 
-@app.route('/flag/<int:campaign_id>', methods=['GET','POST'])
+@app.route('/flag/<int:campaign_id>/', methods=['POST'])
 def flag(campaign_id):
+     
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('UPDATE campaigns SET flagged = 1 WHERE id = ?', (campaign_id,))
+        conn.commit()
+        conn.close
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM campaigns')
+        campaigns = cur.fetchall()
+
+        conn.close()
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM sponsor')
+        sponsor = cur.fetchall()
+        conn.close()
+
+        return render_template("admin_info.html",campaigns=campaigns,sponsor=sponsor)
+
+
+
+@app.route('/unflag/<int:campaign_id>/', methods=['POST'])
+def unflag(campaign_id):
+     
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('UPDATE campaigns SET flagged = 0 WHERE id = ?', (campaign_id,))
+        conn.commit()
+        conn.close
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM campaigns')
+        campaigns = cur.fetchall()
+        conn.close()
+        return render_template("admin_info.html",campaigns=campaigns)
+
+
+
+@app.route('/unflag_sponsor/<string:username>/', methods=['POST'])
+def unflag_sponsor(username):
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('UPDATE campaigns SET flagged = 1 WHERE id = campaign_id')
+    cur.execute('UPDATE sponsor SET flagged = 0 WHERE username = ?', (username,))
     conn.commit()
-    conn.close
+    conn.close()
+
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM campaigns')
-    campaigns = cur.fetchall()
+    cur.execute('SELECT * FROM sponsor')
+    sponsor = cur.fetchall()
+    cur.execute('SELECT * FROM influencer')
+    influencer = cur.fetchall()
     conn.close()
-    return render_template("admin_info.html",campaigns=campaigns)
+
+    return render_template("admin_find_dashboard.html", sponsor=sponsor, influencer=influencer)
+
+
+
+
+@app.route('/flag_sponsor/<string:username>/', methods=['POST'])
+def flag_sponsor(username):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('UPDATE sponsor SET flagged = 1 WHERE username = ?', (username,))
+    conn.commit()
+    conn.close()
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM sponsor')
+    sponsor = cur.fetchall()
+    cur.execute('SELECT * FROM influencer')
+    influencer = cur.fetchall()
+    conn.close()
+
+    return render_template("admin_find_dashboard.html", sponsor=sponsor, influencer=influencer)
+
+
+
+
+@app.route('/unflag_inf/<string:username>/', methods=['POST'])
+def unflag_inf(username):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('UPDATE influencer SET flagged = 0 WHERE username = ?', (username,))
+    conn.commit()
+    conn.close()
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM sponsor')
+    sponsor = cur.fetchall()
+    cur.execute('SELECT * FROM influencer')
+    influencer = cur.fetchall()
+    conn.close()
+
+    return render_template("admin_find_dashboard.html", sponsor=sponsor, influencer=influencer)
+
+
+
+
+@app.route('/flag_inf/<string:username>/', methods=['POST'])
+def flag_inf(username):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('UPDATE influencer SET flagged = 1 WHERE username = ?', (username,))
+    conn.commit()
+    conn.close()
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM sponsor')
+    sponsor = cur.fetchall()
+    cur.execute('SELECT * FROM influencer')
+    influencer = cur.fetchall()
+    conn.close()
+
+    return render_template("admin_find_dashboard.html", sponsor=sponsor, influencer=influencer)
+
+
+
+
+
+
+
+@app.route('/campaign_edit/<int:campaign_id>/<int:sponsor_id>/', methods=['POST'])
+def campaign_edit(campaign_id, sponsor_id):
+    # try:
+        name = request.form['name']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        # budget = request.form['budget']
+        description = request.form['description']
+        visibility = request.form['visibility']
+        #image = None  # assuming image is stored as a blob or filepath
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('''
+            UPDATE campaigns SET name = ?, description = ?, start_date = ?, end_date = ?,visibility =? WHERE id = ?
+        ''', (name, description, start_date, end_date,visibility, campaign_id))
+        
+        conn.commit()
+        conn.close()
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM campaigns')
+        campaigns = cur.fetchall()
+        conn.close()
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM sponsor WHERE id = ?", (sponsor_id,))
+        sponsor = cur.fetchone()
+        conn.close()
+
+        logging.info("Campaign Updated successfully!")
+        return render_template('sponsor_das.html', campaigns=campaigns, sponsor=sponsor)
+    # except Exception as e:
+    #     logging.error(f"Error: {e}")
+
+    #     conn = get_db_connection()
+    #     cur = conn.cursor()
+    #     cur.execute('SELECT * FROM campaigns')
+    #     campaigns = cur.fetchall()
+    #     conn.close()
+
+    #     conn = get_db_connection()
+    #     cur = conn.cursor()
+    #     cur.execute("SELECT * FROM sponsor WHERE id = ?", (sponsor_id,))
+    #     sponsor = cur.fetchone()
+    #     conn.close()
+
+    #     return render_template('sponsor_das.html', campaigns=campaigns, sponsor=sponsor)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
